6ab6ea8160013dd469a23191338e11314c85d7ac
CodeAndroid1590 <tiktoc8@gmail.com>
2019-08-03 07:49:26 -0700

    Android TextView and Button

---
+++ app/src/main/java/com/tiktoc8/textviewandbuttonapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.tiktoc8.textviewandbuttonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button   showBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textview = (TextView)findViewById(R.id.name_txt);
+        showBtn = (Button) findViewById(R.id.show_name);
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview.setText("My name is khan");
+            }
+        });
+    }
+}
